6c27719e6c87376963e67028f5daab7f0d047486
Rusdi Karsandi <rusdi.karsandi@kawanlamaretail.com>
2018-11-28 11:03:14 +0700

    add index.php in root for listing all folder release; add gitignore

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+
+?>
+<html>
+<head>
+	<title>Monix Framework</title>
+</head>
+<body>
+<h1>Monix Framework</h1>
+<p>Monix is a PHP Framework that support MVC and HMVC with ease.</p>
+<div>
+	Release Stable:
+	<ul>
+		<li><a href="1.0">1.0</a></li>
+	</ul>
+</div>
+<hr/>
+
+<div>
+	Release Beta:
+	<ul>
+		<li><a href="1.0">1.0</a></li>
+	</ul>
+</div>
+</body>
+</html>
